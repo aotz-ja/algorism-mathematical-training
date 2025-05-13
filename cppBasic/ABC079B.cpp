e3f8963ba22b75589b9ec19b8a0f56fe2574827d
aotz-ja <mne0mzzzato@gmail.com>
2025-05-13 09:44:20 +0900

    rename

---
+++ cppBasic/ABC079B.cpp
@@ -0,0 +1,24 @@
+//ABC079B
+//入力値 N から N 番目のリュカ数を求める
+//リュカ数の定義は以下
+/*i番目のリュカ数を L_iとすると
+L_0 = 2
+L_1 = 1
+L_i = L_(i-1) + L_(i-2)  (ただし、i≧2)
+と定義される数とする。*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    vector<int64_t> L(N + 1);
+    L.at(0) = 2;
+    L.at(1) = 1;
+    for(int i = 2; i < N + 1; i++){
+        L.at(i) = L.at(i - 2) + L.at(i - 1);
+    }
+    cout << L.at(N) << endl;
+}
