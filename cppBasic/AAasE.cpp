33214eea176a7d1bd58c6f3ca84a5708e07ce88e
aotz-ja <mne0mzzzato@gmail.com>
2025-05-20 15:25:43 +0900

    complete

---
+++ cppBasic/AAasE.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int A,B;
+    char op;
+
+    cin >> A >> op >> B;
+    if(op == '+'){
+        cout << A + B << endl;
+    }
+    else{
+        cout << A - B << endl;
+    }
+
+}
