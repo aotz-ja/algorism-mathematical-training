2fd57c06303cf84277442c2716f36c4742c02ba5
aotz-ja <mne0mzzzato@gmail.com>
2025-05-20 15:01:08 +0900

    complete

---
+++ cppBasic/Aboin.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    char C;
+    cin >> C;
+    if(C == 'a' || C == 'i' || C == 'u' || C == 'e' || C == 'o'){
+        cout << "vowel" << endl;
+    }
+    else{
+        cout << "consonant" << endl;
+    }
+}
