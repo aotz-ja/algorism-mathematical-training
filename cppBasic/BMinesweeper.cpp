814a69fdd0b6138c4e44123c4f4267304e36c8d0
aotz-ja <mne0mzzzato@gmail.com>
2025-05-21 19:18:46 +0900

    char -> string

--- cppBasic/BMinesweeper.cpp
+++ cppBasic/BMinesweeper.cpp
@@ -7,8 +7,7 @@ int main(){
     int H, W;
     cin >> H >> W;
     vector<vector<char>> S(H, vector<char>(W));
-    vector<vector<char>> I(H, vector<char>(W));
-    
+    vector<vector<string>> I(H, vector<string>(W));
     for(int i = 0; i < H; i++){
         for(int j = 0; j < W; j++){
             cin >> S.at(i).at(j);
@@ -19,19 +18,22 @@ int main(){
             if(S.at(i).at(j) == '#'){
                 I.at(i).at(j) = '#';
             }
-            int cnt = 0;
-            for(int k = -1; k < 2; k++){
-                for(int l = -1; l < 2; l++){
-                    if((k == 0 && l == 0) || i+k < 0 || i+k > H-1|| j+l < 0 || j+l > W-1){
-                        break;                            }
-                    else{
-                        if(S.at(i+k).at(j+l) == '#'){
-                        cnt++;
+            else{
+                int cnt = 0;
+                for(int k = -1; k < 2; k++){
+                    for(int l = -1; l < 2; l++){
+                        if((k == 0 && l == 0) || j+l < 0 || j+l > W-1){
+                            continue;
+                        }
+                        else{
+                            if(S.at(i+k).at(j+l) == '#'){
+                            cnt++;
+                            }
                         }
                     }
                 }
+                I.at(i).at(j) = (char)cnt;
             }
-            I.at(i).at(j) = (char)cnt;
         }
     }
     for(int i = 0; i < H; i++){
